ed17272a1a7af3ce4a5afa7918e3ff21309b855c
ssuyash <kumarsuyash94@gmail.com>
2021-01-14 07:41:08 +0530

    form validation

--- js/dom/form.js
+++ js/dom/form.js
@@ -1,14 +1,36 @@
-$(function(){
-    $('form').submit(function(e){
-        e.preventDefault()
+$(function () {
+    $('form').submit(function (e) {
         var username = $('#username').val()
-        if(username.length < 6){
-         $('#username-err').text("Invalid username")
-        }else{
+        var email = $("#email").val()
+        var emailReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if (username.length < 6) {
+            $('#username-err').text("Invalid username")
+            e.preventDefault()
+        } else {
             $('#username-err').text("")
         }
-        
+
+
+        if (!emailReg.test(email)) {
+            $('#email-err').text("Invalid email")
+            e.preventDefault()
+        } else {
+            $('#email-err').text("")
+        }
     })
+
+
+    $("#check").change(function(){
+        var ischeck = $(this).is(':checked')
+        if(ischeck){
+            $("#btn-submit").removeAttr('disabled')
+        }else{
+            $("#btn-submit").attr('disabled', 'disabled')
+        }
+    })
+
+    
 })
 
 
